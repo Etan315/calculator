6cdbfef906dff712304ddbf24a2f649b64bde3ac
Tristan <tumbagatristanehron31@gmail.com>
2026-01-05 21:42:31 +0800

    feat: added event handlers for the calculator

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import "./App.css";
 import { useState } from "react"; 
-import { getRandomNums } from "./js/num-randomizer";
+import { getRandomNums } from "./js/numRandomizer";
+import { solveExpression } from "./js/solveExpression";
 
 function App() {
   const [numbers] = useState(() => getRandomNums());
+  const [display, setDisplay] = useState("");
+
+  const handleNumDisplay = (val) =>{
+    setDisplay(display + val);
+  }
+  
+  const calculateResult = () => {
+    const result = solveExpression(display);
+    setDisplay(result)
+  }
+
+  const clearDisplay = () => {
+    setDisplay("")
+  }
+
+  const deleteOne = () => {
+    const result = display.slice(0, -1);
+    setDisplay(result);
+  }
 
   return (
     <div className="App">
       <div className="calculator-container">
-        <input type="text" className="display-text" />
+        <input type="text" className="display-text" value={display} />
         <div className="btn-container">
-          <button class="btn-clear">CE</button>
+          <button className="btn-clear" onClick={clearDisplay}>CE</button>
+          <button className="btn-del" onClick={deleteOne}>DEL</button>
           <button className="empty"></button>
+          <button className="operation"  onClick={() => handleNumDisplay("+")}>&#43;</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[0])}>{numbers[0]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[1])}>{numbers[1]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[2])}>{numbers[2]}</button>
+          <button className="operation"  onClick={() => handleNumDisplay("-")}>&#45;</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[3])}>{numbers[3]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[4])}>{numbers[4]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[5])}>{numbers[5]}</button>
+          <button className="operation"  onClick={() => handleNumDisplay("×")}>&times;</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[6])}>{numbers[6]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[7])}>{numbers[7]}</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[8])}>{numbers[8]}</button>
+          <button className="operation"  onClick={() => handleNumDisplay("÷")}>&divide;</button>
           <button className="empty"></button>
-          <button className="operation">&#43;</button>
-          <button className="num-slot">{numbers[0]}</button>
-          <button className="num-slot">{numbers[1]}</button>
-          <button className="num-slot">{numbers[2]}</button>
-          <button className="operation">&#45;</button>
-          <button className="num-slot">{numbers[3]}</button>
-          <button className="num-slot">{numbers[4]}</button>
-          <button className="num-slot">{numbers[5]}</button>
-          <button className="operation">&times;</button>
-          <button className="num-slot">{numbers[6]}</button>
-          <button className="num-slot">{numbers[7]}</button>
-          <button className="num-slot">{numbers[8]}</button>
-          <button className="operation">&divide;</button>
-          <button className="empty"></button>
-          <button className="num-slot">{numbers[9]}</button>
-          <button className="dot">.</button>
-          <button className="equal">=</button>
+          <button className="num-slot" onClick={() => handleNumDisplay(numbers[9])}>{numbers[9]}</button>
+          <button className="dot" onClick={() => handleNumDisplay(".")}>.</button>
+          <button className="equal" onClick={calculateResult}>=</button>
         </div>
       </div>
     </div>
